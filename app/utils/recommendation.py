1286f741b15d0ce4c193c8148eca46f2b1e55287
Kaito Hattori <kaichan394@gmail.com>
2021-11-20 14:21:04 +0900

    recommendationを更新

--- app/utils/recommendation.py
+++ app/utils/recommendation.py
@@ -9,22 +9,22 @@ from surprise.model_selection import cross_validate
 class Recommendation(object):
 
     def predict(self, df: pd.DataFrame, dataset_columns: list, item_id: int,
-                user_id: str, limit: int, result_model):
+                user_id: str, limit: int, rating_scale_st: int = 1,
+                rating_scale_end: int = 5, cross_validate_value: int = 2):
 
-        reader = Reader(rating_scale=(1, 5))
+        reader = Reader(rating_scale=(rating_scale_st, rating_scale_end))
         data = Dataset.load_from_df(df[dataset_columns], reader)
-        cross_validate(NormalPredictor(), data, cv=2)
+        cross_validate(NormalPredictor(), data, cv=cross_validate_value)
 
         svd = SVD()
         trainset = data.build_full_trainset()
         svd.fit(trainset)
 
-        items = df.copy()
-        items['Predicted_Score'] = items[item_id].apply(
+        predict_df = df.copy()
+        predict_df['Predicted_Score'] = predict_df[item_id].apply(
             lambda x: svd.predict(user_id, x).est)
 
-        items = items.sort_values(by=['Predicted_Score'], ascending=False)
-        items = items.drop_duplicates(subset=item_id)
-        items.head(limit)
-
-        return [result_model.init_from_df(x) for _, x in items.iterrows()]
+        predict_df = predict_df.sort_values(by=['Predicted_Score'], ascending=False)
+        predict_df = predict_df.drop_duplicates(subset=item_id)
+        predict_df.head(limit)
+        return predict_df
