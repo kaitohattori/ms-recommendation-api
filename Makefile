5a25f4d6cb9c9ee37aec951441c61a3fad6f00c0
Kaito Hattori <kaichan394@gmail.com>
2021-12-04 16:01:47 +0900

    initialize db

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 NAME = ms-recommendation-api
+POSTGRESQL = postgresql
 
 run: ## Run on local
 	python main.py
@@ -8,6 +9,21 @@ docker-build: ## Build on docker
 
 docker-run: ## Run on docker
 	docker run --name $(NAME) --rm -p 8082:8082 $(NAME)
+
+external-run:
+	docker run -d \
+		-p 5432:5432 \
+		-e POSTGRES_DB=video \
+		-e POSTGRES_USER=root \
+		-e POSTGRES_PASSWORD=root \
+		-e POSTGRES_INITDB_ARGS="--encoding=UTF-8" \
+		-v $(PWD)/initdb/:/docker-entrypoint-initdb.d \
+		--name $(POSTGRESQL) \
+		postgres:latest
+
+external-end:
+	docker stop $(POSTGRESQL)
+	docker rm $(POSTGRESQL)
 
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
---
+++ initdb/init.sql
@@ -0,0 +1,45 @@
+-- CREATE DATABASE video;
+\c video
+
+CREATE TABLE videos (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" VARCHAR(50),
+    "title" VARCHAR(50),
+    "create_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE rates (
+    "id" BIGSERIAL UNIQUE NOT NULL,
+    "user_id" VARCHAR(50),
+    "video_id" BIGINT,
+    "value" DECIMAL,
+    "create_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP NOT NULL,
+    PRIMARY KEY ("user_id", "video_id")
+);
+
+CREATE TABLE analyses (
+    "id" BIGSERIAL UNIQUE NOT NULL,
+    "user_id" VARCHAR(50),
+    "video_id" BIGINT,
+    "create_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+INSERT INTO videos VALUES(1, 'user_1', 'title 1', '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO videos VALUES(2, 'user_2', 'title 2', '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO videos VALUES(3, 'user_3', 'title 3', '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO videos VALUES(4, 'user_4', 'title 4', '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+
+INSERT INTO rates VALUES(1, 'user_1', 1, 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO rates VALUES(2, 'user_2', 2, 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO rates VALUES(3, 'user_3', 3, 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO rates VALUES(4, 'user_4', 4, 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+
+INSERT INTO analyses VALUES(1, 'user_1', 1, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO analyses VALUES(2, 'user_2', 2, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO analyses VALUES(3, 'user_3', 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO analyses VALUES(4, 'user_4', 4, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
